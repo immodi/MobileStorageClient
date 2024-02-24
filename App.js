bd93fd0b66a03493f3633548f3a9479fe050b9df
Ahmed Yasser <ay727467@gmail.com>
2024-02-24 14:48:39 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    [color, setColor] = useState('#fff');
+    return (
+        <>
+            <View style={styles(color).container}>
+                <StatusBar style="auto" />
+                <Button title="Color Me!" onPress={() => setColor(getRandomColorHexCode())}/>
+            </View>
+            <View className="flex-1 items-center justify-center">
+            <Text className="text-slate-800">Styling just works! 🎉</Text>
+            </View>
+        </>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+function getRandomColorHexCode() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const styles = (color) => StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: color,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+  
