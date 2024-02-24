a35089de8486582482d49bd29c078873b41a94de
Ahmed Yasser <ay727467@gmail.com>
2024-02-24 16:56:34 +0200

    infinte loop :/

--- App.js
+++ App.js
@@ -5,29 +5,30 @@ import { useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+    
     return (
         <>
-            <FileSystemComponent />
+            {/* infinte loop :/ */}
+            {/* <FileSystemComponent /> */}
             <StatusBar style="auto" />
         </>
     );
 }
 
-const fileSystemData = [
-    { id: '1', name: 'Home', type: 'folder' },
-    { id: '2', name: 'Documents', type: 'folder' },
-    { id: '3', name: 'Downloads', type: 'folder' },
-    { id: '4', name: 'File.txt', type: 'file' },
-];
+// const fileSystemData = [
+//     { id: '1', name: 'Home', type: 'folder' },
+//     { id: '2', name: 'Documents', type: 'folder' },
+//     { id: '3', name: 'Downloads', type: 'folder' },
+//     { id: '4', name: 'File.txt', type: 'file' },
+// ];
 
 // File System Component
 const FileSystemComponent = () => {
     const [selectedItem, setSelectedItem] = useState(null);
-    const [fileSystemData, setFileSystemData] = useState([]);
-    getDirectoryContent(setFileSystemData);
+    const [fileSystemData, setFileSystemData] = useState(null);
     
+    setFileSystemData(getDirectoryContent(setFileSystemData));
     
-
     const handleItemPress = (item) => {        
         setSelectedItem(item);
     };
@@ -63,29 +64,34 @@ const FileSystemComponent = () => {
 };
 
 
-async function getDirectoryContent(setFileSystemData) {
+async function getDirectoryContent() {
     const response = await axios.get(`http://192.168.1.10:8000`, null, { params: {
     }})
     .then(response => {
         let filesArray = response.data.pop()
         let directorysArray = response.data;
+        let dataId = 0
         let fileSystem = []
 
         directorysArray.forEach(directoryObject => {
+            dataId += 1
             fileSystem.push({
+                id: dataId,
                 name: directoryObject.dirPath,
                 type: 'folder'
             })
         });
 
         filesArray.forEach(fileObject => {
+            dataId += 1
             fileSystem.push({
+                id: dataId,
                 name: fileObject.fileName,
                 type: 'file'
             })
         });
 
-        setFileSystemData(fileSystem)
+        return fileSystem
     })
     .catch(err => setFileSystemData([]));
 }
